e383dce91afad7778638848c0f6513ae761b5594
rhys5690 <rhys5690@gmail.com>
2018-04-12 08:51:14 +1000

    Added create action

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -2,4 +2,18 @@ class AccountsController < ApplicationController
  def new
    @account = Account.new
  end
+
+ def create
+   @account = Account.new(account_params)
+   if @account.save
+     redirect_to new_account
+   else
+   end
+ end
+
+ private
+
+  def account_params
+    params.require(:account).permit(:bank_account_total)
+  end
 end
