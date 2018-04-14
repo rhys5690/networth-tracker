c21511b486c21e0c7c3232540a78393134306c80
rhys5690 <rhys5690@gmail.com>
2018-04-14 11:32:10 +1000

    added show page

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -5,6 +5,14 @@ class AccountsController < ApplicationController
 
  def create
    @account = current_user.accounts.create(account_params)
+   if @account.save
+     redirect_to root_path
+   else
+   end
+ end
+
+ def show
+   @bank_account_total = Account.where(user_id: current_user.id).first.bank_account_total
  end
 
  private
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :accounts, only: [:show, :new, :create, :edit]
+
+  root to: 'accounts#show'
 end
